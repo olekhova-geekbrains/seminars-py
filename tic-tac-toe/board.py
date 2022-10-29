165356703ba6c402e97f11c46197609ee366290c
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-10-29 12:01:40 +0300

    add fix

--- tic-tac-toe/board.py
+++ tic-tac-toe/board.py
@@ -9,8 +9,8 @@ BOARD_TEMPLATE = """
 """
 
 
-def create_board(brd: list) -> list:
-    result = BOARD_TEMPLATE
+def create_board(brd: list, board_template: str = BOARD_TEMPLATE) -> list:
+    result = board_template
     for row in brd:
         for cell in row:
             result = result.replace('*', cell, 1)
