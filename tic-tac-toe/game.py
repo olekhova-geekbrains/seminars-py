5ec2d5225c13cdd6c06ed3e95d10c8e8a4bca422
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-10-31 11:08:09 +0300

    add function enter_number

--- tic-tac-toe/game.py
+++ tic-tac-toe/game.py
@@ -26,6 +26,19 @@ def is_valid_number(data: str) -> bool:
     return False
 
 
+def enter_number(phrase: str) -> int:
+    while True:
+        data = input(phrase)
+        if data == '':
+            return -1
+        if is_valid_number(data):
+            num = int(data) - 1
+            break
+        else:
+            print('Неправильный ввод. Введите еще раз!')
+    return num
+
+
 def play_ttt(user_x: str, user_o: str) -> str:
     current_player = select_player(user_x, user_o)
     print(f"Первый ходит {current_player}")
@@ -35,34 +48,26 @@ def play_ttt(user_x: str, user_o: str) -> str:
     symbol_x = 'x'
     symbol_o = 'o'
     while True:
-        if not has_empty_cell(game_progress): 
+        if not has_empty_cell(game_progress):
             print('Все клетки заполнены.')
             break
         print(f'Ходит игрок {current_player}.')
-        while True:
-            data = input('Введите номер строки 1, 2 или 3: ')
-            if data == '':
-                return f'Игрок {current_player} сдался'
-            if is_valid_number(data):
-                i = int(data) - 1
-                break
-            else:
-                print('Неправильный ввод. Введите еще раз!')
-        while True:
-            data = input('Введите номер столбца 1, 2 или 3: ')
-            if data == '':
-                return f'Игрок {current_player} сдался'
-            if is_valid_number(data):
-                j = int(data) - 1
-                break
-            else:
-                print('Неправильный ввод. Введите еще раз!')
+
+        i = enter_number('Введите номер строки 1, 2 или 3: ')
+        if i == -1:
+            return f'Игрок {current_player} сдался'
+
+        j = enter_number('Введите номер стобца 1, 2 или 3: ')
+        if j == -1:
+            return f'Игрок {current_player} сдался'
+
         if is_empty_cell(game_progress, i, j):
             game_progress[i][j] = symbol_x if current_player == user_x else symbol_o
             current_player = user_o if current_player == user_x else user_x
             current_board = board.create_board(game_progress)
         else:
             print('Ячейка занята! Переходите.')
+
         print(current_board)
     return 'Конец игры'
 
@@ -70,6 +75,5 @@ def play_ttt(user_x: str, user_o: str) -> str:
 if __name__ == "__main__":
     name_x = input('Введите имя игрока, который ходит "x": ')
     name_o = input('Введите имя игрока, который ходит "o": ')
-
 
     print(play_ttt(name_x, name_o))
