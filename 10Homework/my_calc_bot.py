eba3f03ad30105ba236c7b12605659a3feeb6409
Lyubov Olekhova <olekhova+geekbrains@gmail.com>
2022-11-29 13:53:20 +0300

    add calc

--- 10Homework/my_calc_bot.py
+++ 10Homework/my_calc_bot.py
@@ -3,8 +3,9 @@ from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 import config
-# Define a few command handlers. These usually take the two arguments update and
-# context.
+import calc_model
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     """Send a message when the command /start is issued."""
@@ -13,10 +14,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_html(
 
-        rf"Hi {user.mention_html()}!",
-
-        reply_markup=ForceReply(selective=True),
-
+        rf"""
+    Hi {user.mention_html()}!
+    Это бот-калькулятор
+    Введите математическое выражение
+    вида `/calc выражение`""",
     )
 
 
@@ -24,35 +26,23 @@ async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> No
 
     """Send a message when the command /help is issued."""
 
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("Введите команду вида `/calc выражение`")
 
 
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def calc_response(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
-    """Echo the user message."""
-
-    await update.message.reply_text(update.message.text)
+    await update.message.reply_text(calc_model.calculate(update.message.text[6:]))
 
 
 def main() -> None:
     """Start the bot."""
     bot_token = config.TOKEN
-    # Create the Application and pass it your bot's token.
 
     application = Application.builder().token(bot_token).build()
 
-    # on different commands - answer in Telegram
-
     application.add_handler(CommandHandler("start", start))
-
     application.add_handler(CommandHandler("help", help_command))
-
-    # on non command i.e message - echo the message on Telegram
-
-    application.add_handler(MessageHandler(
-        filters.TEXT & ~filters.COMMAND, echo))
-
-    # Run the bot until the user presses Ctrl-C
+    application.add_handler(CommandHandler("calc", calc_response))
 
     application.run_polling()
 
